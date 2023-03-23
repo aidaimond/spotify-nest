1e38ba1521e926bc2b1547c983ec899bd60c2557
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-03-23 16:08:38 +0600

    Created requests for artists. Installed types for multer.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { Model } from 'mongoose';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateArtistDto } from './create-artist.dto';
 
 @Controller('artists')
-export class ArtistsController {}
+export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name)
+    private artistModel: Model<ArtistDocument>,
+  ) {}
+
+  @Get()
+  async getAll() {
+    return this.artistModel.find();
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    return this.artistModel.findOne({ _id: id });
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/products' }),
+  )
+  async createArtist(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistDto: CreateArtistDto,
+  ) {
+    const artist = new this.artistModel({
+      name: artistDto.name,
+      info: artistDto.info,
+      image: file ? '/uploads/artists/' + file.filename : null,
+    });
+    return artist.save();
+  }
+}
