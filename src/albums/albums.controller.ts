cfffb9803600e775f3ebb78194d9baa521cd7a2c
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-03-24 14:24:00 +0600

    Added delete in albums. Created requests for tracks

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,34 +1,47 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
   Query,
   UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Album, AlbumDocument } from '../schemas/album.schema';
 import { Model } from 'mongoose';
 import { CreateAlbumDto } from './create-album.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Track, TrackDocument } from '../schemas/track.schema';
 
 @Controller('albums')
 export class AlbumsController {
   constructor(
     @InjectModel(Album.name)
     private albumModel: Model<AlbumDocument>,
+    @InjectModel(Track.name)
+    private trackModel: Model<TrackDocument>,
   ) {}
   @Get()
-  async getAll(@Query() artistId: { [key: string]: string }) {
-    return console.log(artistId.artist);
+  getAll(@Query() artistId: { [key: string]: string }) {
+    if (artistId.artist) {
+      const id = artistId.artist;
+      return this.albumModel.find({ artist: id });
+    }
+    return this.albumModel.find();
   }
 
-  @Get()
-  async getOne(@Param('id') id: string) {
-    return this.albumModel.findById({ _id: id });
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.albumModel.findById(id);
   }
 
   @Post()
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/albums' }),
+  )
   async createAlbum(
     @UploadedFile() file: Express.Multer.File,
     @Body() albumDto: CreateAlbumDto,
@@ -36,9 +49,24 @@ export class AlbumsController {
     const album = new this.albumModel({
       name: albumDto.name,
       artist: albumDto.artist,
-      yearOfIssue: albumDto.yearOfIssue,
-      image: file ? '/uploads/products/' + file.filename : null,
+      yearOfIssue: parseInt(albumDto.yearOfIssue),
+      image: file ? '/uploads/albums/' + file.filename : null,
     });
-    return album.save();
+    return await album.save();
+  }
+
+  @Delete(':id')
+  async deleteAlbum(@Param('id') id: string) {
+    const album = await this.albumModel.findById(id);
+    if (album) {
+      const tracks = await this.trackModel.find({ album: id });
+      if (tracks.length) {
+        return { message: "You can't delete an album if they have tracks" };
+      }
+      await this.albumModel.deleteOne({ _id: id });
+
+      return { message: 'Album deleted' };
+    }
+    return { message: 'Album not found' };
   }
 }
--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Track, TrackDocument } from '../schemas/track.schema';
 import { Model } from 'mongoose';
+import { CreateTrackDto } from './create-track.dto';
 
 @Controller('tracks')
 export class TracksController {
@@ -9,8 +18,31 @@ export class TracksController {
     @InjectModel(Track.name)
     private trackModel: Model<TrackDocument>,
   ) {}
+
   @Get()
-  async getAll() {
+  getAll(@Query() albumId: { [key: string]: string }) {
+    if (albumId.album) {
+      return this.trackModel.find({ album: albumId.album });
+    }
     return this.trackModel.find();
+  }
+
+  @Post()
+  async createTrack(@Body() trackDto: CreateTrackDto) {
+    const track = new this.trackModel({
+      name: trackDto.name,
+      album: trackDto.album,
+      duration: trackDto.duration,
+    });
+    return await track.save();
+  }
+
+  @Delete(':id')
+  deleteTrack(@Param('id') id: string) {
+    const track = this.trackModel.findById(id);
+    if (track) {
+      return this.trackModel.deleteOne({ _id: id });
+    }
+    return { message: 'Track not found' };
   }
 }
