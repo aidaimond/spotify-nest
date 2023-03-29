00990c16136c0fc5b73bbf922f89d7621c4104ed
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-03-29 15:20:39 +0600

    #HW-94 Created logout and login. Added service, guard and strategy for auth.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -18,12 +19,31 @@ export class UsersController {
       password: req.body.password,
 
       displayName: req.body.displayName,
-
-      role: req.body.role,
     });
 
     user.generateToken();
 
     return user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  login(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Delete('sessions')
+  async logout(@Req() req: Request) {
+    const token = req.get('Authorization');
+    if (!token) {
+      return { message: 'No token' };
+    }
+    const user = (await this.userModel.findOne({ token })) as UserDocument;
+    if (!user) {
+      return { message: 'User not found' };
+    }
+    await user.generateToken();
+    await user.save();
+    return { message: 'Logout successfully' };
   }
 }
